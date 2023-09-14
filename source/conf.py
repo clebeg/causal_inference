bd52aba2b6a9ee7bae85179541d9cf426d1530a4
clebegxie(谢慧志) <clebegxie@tencent.com>
2023-09-14 20:47:24 +0800

    first commit

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '因果推断实战'
+copyright = '2023, cleverbegin'
+author = 'cleverbegin'
+release = 'v1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['recommonmark', 'sphinx_markdown_tables']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'insegel'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. CausalInference documentation master file, created by
+   sphinx-quickstart on Thu Sep 14 20:28:21 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+欢迎查看【因果推断实战】教程!
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+索引
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
